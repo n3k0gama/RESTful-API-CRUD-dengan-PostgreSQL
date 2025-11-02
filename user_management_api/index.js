6d78591e97c493f2568765d7fc5751bf35759f00
Dyo Wiratama <144615306+n3k0gama@users.noreply.github.com>
2025-11-03 05:30:06 +0700

    fix bug

--- user_management_api/index.js
+++ user_management_api/index.js
@@ -7,9 +7,10 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use('/uploads', express.static('uploads')); // ← penting
+app.use('/uploads', express.static('uploads')); 
 
 app.use('/api/users', userRoutes);
+app.use('/api', authRoutes);
 
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
